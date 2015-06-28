7dbe98a92fa7df3f4ebce1c0598157efdcbde4f4
Soth02 <cpfong@gmail.com>
2015-06-27 23:51:30 -0700

    testing

--- Code/getTranslation.js
+++ Code/getTranslation.js
@@ -12,7 +12,6 @@ var url="https://www.idolondemand.com/sample-content/videos/hpnext.mp4";
 url = url.replace(/\//g, '%2F');
 url=url.replace(":","%3A");
 console.log(url);
-//https.get('https://api.idolondemand.com/1/api/async/recognizespeech/v1?url=http%3A%2F%2Fwww.talkenglish.com%2FAudioTE%2FL21%2Fsentence%2FL21S5.mp3&interval=0&apikey=f3129194-4f03-4419-80c2-f3aa041baf9a',function(res){
 https.get('https://api.idolondemand.com/1/api/async/recognizespeech/v1?url='+url+'&apikey=dc8c38c1-730d-4c5e-ac0d-8c9c82932bf4',function(res){
 
 var data='';
