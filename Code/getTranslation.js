b159274156ff0a31a9ebca973145694d971b5784
Soth02 <cpfong@gmail.com>
2015-06-28 01:04:05 -0700

    the input file is test.mp3, output JSON is test.txt

--- Code/getTranslation.js
+++ Code/getTranslation.js
@@ -5,25 +5,37 @@
 
 var cors = require('cors')
 var express=require('express');
+var needle = require('needle');
+var fs = require('fs');
+
 var app = express();
 app.use(cors());
 var https=require('https');
-var url="https://www.idolondemand.com/sample-content/videos/hpnext.mp4";
-url = url.replace(/\//g, '%2F');
-url=url.replace(":","%3A");
-console.log(url);
-https.get('https://api.idolondemand.com/1/api/async/recognizespeech/v1?url='+url+'&apikey=dc8c38c1-730d-4c5e-ac0d-8c9c82932bf4',function(res){
+//var url="https://www.idolondemand.com/sample-content/videos/hpnext.mp4";
+//url = url.replace(/\//g, '%2F');
+//url=url.replace(":","%3A");
+//console.log(url);
 
-var data='';
-res.on('data', function(d) {
-    data+=d;
-});
-res.on('end',function(data1){
-    console.log(data);
-    data=JSON.parse(data);
-    console.log(data);
-    jobID=data.jobID;
-     https.get('https://api.idolondemand.com/1/job/result/'+jobID+'?apikey=dc8c38c1-730d-4c5e-ac0d-8c9c82932bf4',function(res){
+var apiurl='https://api.idolondemand.com/1/api/async/recognizespeech/v1'
+var apikey='dc8c38c1-730d-4c5e-ac0d-8c9c82932bf4'
+
+//var url  = 'http://posttestserver.com/post.php?dir=needle';
+var url  = apiurl;
+var soundfile = 'test.mp3'
+
+var data = {
+  apikey: apikey,
+  file: {
+	//'file': 'hpnext.mp4',
+	'file': soundfile,
+	 'content_type': 'multipart/form-data' }
+}
+
+var resp = needle.post(url, data, { multipart: true }, function(err, resp) {
+	if (!err && resp.statusCode == 200)
+    		console.log(resp.body.jobID); // here you go, mister.
+
+	https.get('https://api.idolondemand.com/1/job/result/'+resp.body.jobID+'?apikey=dc8c38c1-730d-4c5e-ac0d-8c9c82932bf4',function(res){
 
         var dt1='';
         res.on('data', function(dt) {
@@ -31,10 +43,19 @@ res.on('end',function(data1){
         });
         res.on('end',function(dtt){
             console.log(dt1);
+
+	    fs.writeFile("test.txt", dt1, function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+
+                console.log("The file was saved!");
+            }); 
+
         });
+
     });
-});
-
-   /**/
 
 });
+
+
